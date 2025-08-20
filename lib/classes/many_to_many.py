aa55cb618c1737ae2ad66773ab1a38d73f409a2a
Francis <franciskarani15@gmail.com>
2025-08-20 23:24:49 +0300

    initial commit

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,38 +1,118 @@
-class Article:
-    def __init__(self, author, magazine, title):
-        self.author = author
-        self.magazine = magazine
-        self.title = title
-        
 class Author:
     def __init__(self, name):
-        self.name = name
+        if not isinstance(name, str) or len(name.strip()) == 0:
+            raise ValueError("Author name must be a non-empty string")
+        self._name = name
+        self._articles = []
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        # ignore attempts to change name (immutable)
+        pass
 
     def articles(self):
-        pass
+        return self._articles
 
     def magazines(self):
-        pass
+        return list({article.magazine for article in self._articles})
 
     def add_article(self, magazine, title):
-        pass
+        # Article constructor will handle appending to author and magazine
+        return Article(self, magazine, title)
 
     def topic_areas(self):
-        pass
+        if not self._articles:
+            return None
+        return list({mag.category for mag in self.magazines()})
+
 
 class Magazine:
     def __init__(self, name, category):
-        self.name = name
-        self.category = category
+        self._name = name
+        self._category = category
+        self._articles = []
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and (2 <= len(value) <= 16):
+            self._name = value
+        # ignore invalid changes
+
+    @property
+    def category(self):
+        return self._category
+
+    @category.setter
+    def category(self, value):
+        if isinstance(value, str) and len(value.strip()) > 0:
+            self._category = value
+        # ignore invalid changes
 
     def articles(self):
-        pass
+        return self._articles
 
     def contributors(self):
-        pass
+        return list({article.author for article in self._articles})
 
     def article_titles(self):
-        pass
+        if not self._articles:
+            return None
+        return [article.title for article in self._articles]
 
     def contributing_authors(self):
+        author_count = {}
+        for article in self._articles:
+            author_count[article.author] = author_count.get(
+                article.author, 0) + 1
+        return [author for author, count in author_count.items() if count > 2] or None
+
+
+class Article:
+    all = []
+
+    def __init__(self, author, magazine, title):
+        if not isinstance(title, str) or not (5 <= len(title) <= 50):
+            raise ValueError(
+                "Title must be a string between 5 and 50 characters")
+        self._title = title
+        self._author = author
+        self._magazine = magazine
+
+        author._articles.append(self)
+        magazine._articles.append(self)
+        Article.all.append(self)
+
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, value):
+        # ignore attempts to change title (immutable)
         pass
+
+    @property
+    def author(self):
+        return self._author
+
+    @author.setter
+    def author(self, value):
+        if isinstance(value, Author):
+            self._author = value
+
+    @property
+    def magazine(self):
+        return self._magazine
+
+    @magazine.setter
+    def magazine(self, value):
+        if isinstance(value, Magazine):
+            self._magazine = value
